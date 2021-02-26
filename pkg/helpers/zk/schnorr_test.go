3b42312579fc6af9b0baa23af2d6a98f040c14bc
Adrian Hamelink <adrian.hamelink@gmail.com>
2021-02-26 18:03:29 +0000

    Proper check of cofactor for order 8 point

--- pkg/helpers/zk/schnorr_test.go
+++ pkg/helpers/zk/schnorr_test.go
@@ -1,10 +1,11 @@
 package zk
 
 import (
-	"fmt"
+	"encoding/hex"
 	"testing"
 
 	"filippo.io/edwards25519"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/taurusgroup/frost-ed25519/pkg/helpers/scalar"
 )
@@ -20,21 +21,6 @@ func TestSchnorrProof(t *testing.T) {
 	require.True(t, proof.Verify(partyID, public, ctx))
 }
 
-func TestSchnorrProofFail(t *testing.T) {
-	partyID := uint32(42)
-
-	secret := edwards25519.NewScalar()
-	public := new(edwards25519.Point).ScalarBaseMult(secret)
-
-	ctx := make([]byte, 32)
-	proof := NewSchnorrProof(partyID, public, ctx, secret)
-	data, _ := proof.MarshalBinary()
-	var proof2 Schnorr
-	proof2.UnmarshalBinary(data)
-
-	require.False(t, proof2.Verify(partyID, public, ctx))
-}
-
 func TestSchnorrCofactor(t *testing.T) {
 	partyID := uint32(42)
 
@@ -43,10 +29,33 @@ func TestSchnorrCofactor(t *testing.T) {
 
 	ctx := make([]byte, 32)
 	proof := NewSchnorrProof(partyID, public, ctx, secret)
-	data, _ := proof.MarshalBinary()
-	var proof2 Schnorr
-	proof2.UnmarshalBinary(data)
-	fmt.Println(1, proof.S.Bytes(), proof.R.Bytes())
-	fmt.Println(2, proof2.S.Bytes(), proof2.R.Bytes())
-	require.False(t, proof2.Verify(partyID, public, ctx))
+
+	for i, T := range order8 {
+		var publicPrime edwards25519.Point
+		// TODO This should not work
+		if T.Equal(edwards25519.NewIdentityPoint()) != 1 {
+			assert.False(t, proof.Verify(partyID, publicPrime.Add(public, T), ctx), i)
+		}
+	}
+}
+
+var order8 []*edwards25519.Point
+
+func init() {
+	order8Hex := []string{
+		"0100000000000000000000000000000000000000000000000000000000000000",
+		"ECFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF7F",
+		"0000000000000000000000000000000000000000000000000000000000000080",
+		"0000000000000000000000000000000000000000000000000000000000000000",
+		"C7176A703D4DD84FBA3C0B760D10670F2A2053FA2C39CCC64EC7FD7792AC037A",
+		"C7176A703D4DD84FBA3C0B760D10670F2A2053FA2C39CCC64EC7FD7792AC03FA",
+		"26E8958FC2B227B045C3F489F2EF98F0D5DFAC05D3C63339B13802886D53FC05",
+		"26E8958FC2B227B045C3F489F2EF98F0D5DFAC05D3C63339B13802886D53FC85",
+	}
+	order8 = make([]*edwards25519.Point, 0, 8)
+	for _, h := range order8Hex {
+		b, _ := hex.DecodeString(h)
+		p, _ := new(edwards25519.Point).SetBytes(b)
+		order8 = append(order8, p)
+	}
 }
