78a0f348f1439f78f6635d9f6208066026a444aa
JP Aumasson <jpa@pm.me>
2021-03-08 15:18:18 +0100

    keygen

--- cmd/keygen/keygen.go
+++ cmd/keygen/keygen.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strconv"
 
+	"github.com/taurusgroup/frost-ed25519/pkg/eddsa"
 	"github.com/taurusgroup/frost-ed25519/pkg/frost"
+	"github.com/taurusgroup/frost-ed25519/pkg/frost/keygen"
 	"github.com/taurusgroup/frost-ed25519/pkg/frost/party"
 	"github.com/taurusgroup/frost-ed25519/pkg/helpers"
+	"github.com/taurusgroup/frost-ed25519/pkg/state"
 )
 
 const maxN = 100
@@ -48,9 +52,59 @@ func main() {
 
 	partySet := helpers.GenerateSet(party.ID(n))
 
-	for id := range partySet.Range() {
-		var err error
-		states[id], outputs[id], err = frost.NewKeygenState(id, partySet, party.Size(t), 0)
+	// structure holding parties' state and output
+	states := map[party.ID]*state.State{}
+	outputs := map[party.ID]*keygen.Output{}
 
+	// create a state for eahc party
+	for id := range partySet.Range() {
+		states[id], outputs[id], err = frost.NewKeygenState(id, partySet, party.Size(t), 0)
 	}
+
+	msgsOut1 := make([][]byte, 0, n)
+	msgsOut2 := make([][]byte, 0, n*(n-1)/2)
+
+	for _, s := range states {
+		msgs1, err := helpers.PartyRoutine(nil, s)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		msgsOut1 = append(msgsOut1, msgs1...)
+	}
+
+	for _, s := range states {
+		msgs2, err := helpers.PartyRoutine(msgsOut1, s)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		msgsOut2 = append(msgsOut2, msgs2...)
+	}
+
+	for _, s := range states {
+		_, err := helpers.PartyRoutine(msgsOut2, s)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	secrets := map[party.ID]*eddsa.SecretShare{}
+
+	fmt.Println("Secret shares:")
+	for id := range partySet.Sorted() {
+		Id := party.ID(id + 1)
+		if err := states[Id].WaitForError(); err != nil {
+			fmt.Println(err)
+			return
+		}
+		secrets[Id] = outputs[Id].SecretKey
+		fmt.Printf("%v", outputs[Id].SecretKey.ID)
+		fmt.Printf(": %v\n", hex.EncodeToString(outputs[Id].SecretKey.Scalar().Bytes()))
+	}
+
+	// TODO: write JSON file, to take as input by CLI signer
+
+	fmt.Println("OK")
 }
