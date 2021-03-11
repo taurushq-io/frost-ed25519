ecb4c50282c3e53acd1e51cd469244c93e811837
Adrian Hamelink <adrian.hamelink@gmail.com>
2021-03-11 16:33:07 +0000

    eddsa/secret_share: add marshal test

--- pkg/eddsa/secret_share_test.go
+++ pkg/eddsa/secret_share_test.go
@@ -1,3 +1,43 @@
 package eddsa
 
+import (
+	"testing"
+
+	"github.com/taurusgroup/frost-ed25519/pkg/internal/scalar"
+)
+
 // TODO write marshalling test
+
+func TestSecretShare_MarshalJSON(t *testing.T) {
+	secret := scalar.NewScalarUInt32(42)
+	s := NewSecretShare(42, secret)
+	dataJson, err := s.MarshalJSON()
+	if err != nil {
+		t.Error(err)
+	}
+	var s2 SecretShare
+	err = s2.UnmarshalJSON(dataJson)
+	if err != nil {
+		t.Error(err)
+	}
+	if !s2.Equal(s) {
+		t.Error("unmarshalled share is not the same")
+	}
+}
+
+func TestSecretShare_MarshalBinary(t *testing.T) {
+	secret := scalar.NewScalarUInt32(42)
+	s := NewSecretShare(42, secret)
+	dataBin, err := s.MarshalBinary()
+	if err != nil {
+		t.Error(err)
+	}
+	var s2 SecretShare
+	err = s2.UnmarshalBinary(dataBin)
+	if err != nil {
+		t.Error(err)
+	}
+	if !s2.Equal(s) {
+		t.Error("unmarshalled share is not the same")
+	}
+}
