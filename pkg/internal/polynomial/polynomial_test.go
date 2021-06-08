3a6d9bc7f376732dd6d9a7a6eaade0d2e4c94d01
Adrian Hamelink <adrian.hamelink@gmail.com>
2021-06-08 16:05:58 +0200

    better polynomial test

--- pkg/internal/polynomial/polynomial_test.go
+++ pkg/internal/polynomial/polynomial_test.go
@@ -1,9 +1,10 @@
 package polynomial
 
 import (
+	"encoding/binary"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
 	"github.com/taurusgroup/frost-ed25519/pkg/frost/party"
 	"github.com/taurusgroup/frost-ed25519/pkg/internal/scalar"
 	"github.com/taurusgroup/frost-ed25519/pkg/ristretto"
@@ -12,19 +13,17 @@ import (
 func TestPolynomial_Evaluate(t *testing.T) {
 	{
 		polynomial := &Polynomial{make([]ristretto.Scalar, 3)}
-		polynomial.coefficients[0].Set(scalar.NewScalarUInt32(1))
-		polynomial.coefficients[2].Set(scalar.NewScalarUInt32(1))
+		polynomial.coefficients[0] = *scalar.NewScalarUInt32(1)
+		polynomial.coefficients[1] = *scalar.NewScalarUInt32(0)
+		polynomial.coefficients[2] = *scalar.NewScalarUInt32(1)
 
 		for index := uint32(0); index < 100; index++ {
 			x := party.RandID()
-			max := 1 << 8 * int64(party.ByteSize)
-			if int64(x) > max {
-				continue
-			}
-			result := 1 + x*x
-			computedResult := polynomial.Evaluate(party.ID(index).Scalar())
-			expectedResult := result.Scalar()
-			require.Equal(t, 1, expectedResult.Equal(computedResult))
+			xUint := uint64(x)
+			expectedResult := 1 + xUint*xUint
+			computedResultScalar := polynomial.Evaluate(x.Scalar())
+			computedResult := binary.LittleEndian.Uint64(computedResultScalar.Bytes())
+			assert.Equal(t, int(expectedResult), int(computedResult))
 		}
 	}
 }
