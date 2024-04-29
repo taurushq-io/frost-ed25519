316941d29ff3c93bcfe434981eb084a938e61dda
Woods <woods@bipal.com>
2024-04-29 10:58:55 +0800

    分片拆分和验证

--- woods/main.go
+++ woods/main.go
@@ -555,6 +555,284 @@ func verifyKeysV2(filename string, msg string, rn int) {
 
 }
 
+func keygenDemoV3(t int, n int) [][]byte {
+
+	var err error
+	if (n > maxN) || (t >= n) {
+		usage()
+		return nil
+	}
+
+	partyIDs := helpers.GenerateSet(party.ID(n))
+
+	// structure holding parties' state and output
+	states := map[party.ID]*state.State{}
+	outputs := map[party.ID]*keygen.Output{}
+
+	// create a state for each party
+	for _, id := range partyIDs {
+		states[id], outputs[id], err = frost.NewKeygenState(id, partyIDs, party.Size(t), 0)
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+	}
+
+	msgsOut1 := make([][]byte, 0, n)
+	msgsOut2 := make([][]byte, 0, n*(n-1)/2)
+
+	for _, s := range states {
+		msgs1, err := helpers.PartyRoutine(nil, s)
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		msgsOut1 = append(msgsOut1, msgs1...)
+	}
+
+	for _, s := range states {
+		msgs2, err := helpers.PartyRoutine(msgsOut1, s)
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		msgsOut2 = append(msgsOut2, msgs2...)
+	}
+
+	for _, s := range states {
+		_, err := helpers.PartyRoutine(msgsOut2, s)
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+	}
+
+	// Get the public data
+	fmt.Println("Group Key:")
+	id0 := partyIDs[0]
+	if err = states[id0].WaitForError(); err != nil {
+		fmt.Println(err)
+		return nil
+	}
+	public := outputs[id0].Public
+	secrets := make(map[party.ID]*eddsa.SecretShare, n)
+	groupKey := public.GroupKey
+	fmt.Printf("  %x\n\n", groupKey.ToEd25519())
+
+	for _, id := range partyIDs {
+		if err := states[id].WaitForError(); err != nil {
+			fmt.Println(err)
+			return nil
+		}
+		shareSecret := outputs[id].SecretKey
+		sharePublic := public.Shares[id]
+		secrets[id] = shareSecret
+		fmt.Printf("Party %d:\n  secret: %x\n  public: %x\n", id, shareSecret.Secret.Bytes(), sharePublic.Bytes())
+	}
+
+	// TODO: write JSON file, to take as input by CLI signer
+	type KeyGenOutput struct {
+		Secrets map[party.ID]*eddsa.SecretShare
+		Shares  *eddsa.Public
+	}
+
+	var slices [][]byte
+	for _, id := range partyIDs {
+
+		// 创建一个新的 map 用于存储过滤后的 secretShare
+		filteredSecrets := make(map[party.ID]*eddsa.SecretShare)
+
+		// 遍历原始的 secrets map
+		for nid, secret := range secrets {
+			// 如果ID不等于'2'，则将其添加到新的 map 中
+			if nid == id {
+				filteredSecrets[id] = secret
+			}
+		}
+
+		filteredShares := make(map[party.ID]*ristretto.Element)
+		filteredShares[id] = public.Shares[id]
+
+		filteredPubs := &eddsa.Public{
+			partyIDs,
+			party.Size(t),
+			filteredShares,
+			public.GroupKey,
+		}
+
+		kgOutput := KeyGenOutput{
+			Secrets: filteredSecrets,
+			Shares:  filteredPubs,
+		}
+		var jsonData []byte
+		jsonData, err = json.MarshalIndent(kgOutput, "", " ")
+		if err != nil {
+			fmt.Println(err)
+			return nil
+		}
+
+		slices = append(slices, jsonData)
+
+	}
+
+	fmt.Println("生成分片：-----------------------")
+	fmt.Println(slices)
+	return slices
+
+}
+
+func mergeJsonV3(slices [][]byte) ([]byte, error) {
+	combinedOutput := CombinedOutput{
+		Secrets: make(map[string]Secret),
+		Shares:  Shares{Shares: make(map[string]string)},
+	}
+
+	var err error
+	for i := 0; i < len(slices); i++ {
+		data := slices[i]
+
+		var output CombinedOutput
+		err = json.Unmarshal(data, &output)
+		if err != nil {
+			return nil, err
+		}
+
+		// Merge the secrets from each file
+		for key, secret := range output.Secrets {
+			combinedOutput.Secrets[key] = secret
+		}
+
+		// Merge the shares from each file
+		for key, value := range output.Shares.Shares {
+			combinedOutput.Shares.Shares[key] = value
+		}
+
+		// Update other fields if needed
+		if combinedOutput.Shares.T == 0 {
+			combinedOutput.Shares.T = output.Shares.T
+			combinedOutput.Shares.GroupKey = output.Shares.GroupKey
+		}
+	}
+
+	combinedJSON, err := json.MarshalIndent(combinedOutput, "", "  ")
+	if err != nil {
+		return nil, err
+	}
+
+	return combinedJSON, nil
+}
+
+func verifyKeysV3(slices [][]byte, msg string) {
+
+	message := []byte(msg)
+
+	var err error
+
+	type KeyGenOutput struct {
+		Secrets map[party.ID]*eddsa.SecretShare
+		Shares  *eddsa.Public
+	}
+
+	mjson, err := mergeJsonV3(slices)
+
+	fmt.Println("msg: ", len(message))
+	fmt.Println("merged: ", string(mjson))
+	fmt.Println("error: ", err)
+
+	var kgOutput KeyGenOutput
+
+	var jsonData []byte = mjson
+	//jsonData, err = ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("json: --- ", string(mjson), err)
+
+	err = json.Unmarshal(jsonData, &kgOutput)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// get n and t from the keygen output
+	var n party.Size
+	var t party.Size
+
+	n = kgOutput.Shares.PartyIDs.N()
+	t = kgOutput.Shares.Threshold
+
+	fmt.Printf("(t, n) = (%v, %v)\n", t, n)
+
+	partyIDs := helpers.GenerateSet(n)
+
+	secretShares := kgOutput.Secrets
+	publicShares := kgOutput.Shares
+
+	// structure holding parties' state and output
+	states := map[party.ID]*state.State{}
+	outputs := map[party.ID]*sign.Output{}
+
+	msgsOut1 := make([][]byte, 0, n)
+	msgsOut2 := make([][]byte, 0, n)
+
+	for _, id := range partyIDs {
+		states[id], outputs[id], err = frost.NewSignState(partyIDs, secretShares[id], publicShares, message, 0)
+		if err != nil {
+			fmt.Println()
+		}
+	}
+
+	pk := publicShares.GroupKey
+
+	for _, s := range states {
+		msgs1, err := helpers.PartyRoutine(nil, s)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		msgsOut1 = append(msgsOut1, msgs1...)
+	}
+
+	for _, s := range states {
+		msgs2, err := helpers.PartyRoutine(msgsOut1, s)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		msgsOut2 = append(msgsOut2, msgs2...)
+	}
+
+	for _, s := range states {
+		_, err := helpers.PartyRoutine(msgsOut2, s)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	id0 := partyIDs[0]
+	sig := outputs[id0].Signature
+	if sig == nil {
+		fmt.Println("null signature")
+		return
+	}
+
+	if !ed25519.Verify(pk.ToEd25519(), message, sig.ToEd25519()) {
+		fmt.Println("signature verification failed (ed25519)")
+		return
+	}
+
+	if !pk.Verify(message, sig) {
+		fmt.Println("signature verification failed")
+		return
+	}
+
+	fmt.Printf("Success: signature is\nr: %x\ns: %x\n", sig.R.Bytes(), sig.S.Bytes())
+
+}
+
 func main() {
 	//keygenDemo(2, 3)
 
@@ -562,5 +840,8 @@ func main() {
 	//verifyKeysV2("/Users/wuxi/code/mine/frost-ed25519/woods/keygenout3.json", "message_test111")
 	//keygenDemoV2(2, 3)
 
-	verifyKeysV2("/Users/wuxi/code/mine/frost-ed25519/keygenout", "message222", 3)
+	//verifyKeysV2("/Users/wuxi/code/mine/frost-ed25519/keygenout", "message222", 3)
+
+	slices := keygenDemoV3(2, 3)
+	verifyKeysV3(slices, "message222")
 }
